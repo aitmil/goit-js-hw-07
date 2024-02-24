77e19422ee0f2b662cb69d675c58eb8c612d1e1a
aitmil <120422761+aitmil@users.noreply.github.com>
2024-02-24 11:38:57 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,17 @@
+const listItems = document.querySelectorAll(".item");
+console.log(`Number of categories: ${listItems.length}`);
 
+listItems.forEach((item) => {
+  const firstChild = item.firstElementChild.textContent;
+  const lastChild = item.lastElementChild.children.length;
+  console.log(`Category: ${firstChild}`);
+  console.log(`Elements: ${lastChild}`);
+});
+
+// const titles = document.querySelectorAll("h2");
+// titles.forEach((title) => {
+//   console.log(`Category: ${title.textContent}`);
+
+//   const itemsCategory = title.parentElement.querySelectorAll("ul>li");
+//   console.log(`Elements: ${itemsCategory.length}`);
+// });
