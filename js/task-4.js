072f5a5930da6567b1ced5990ac8e100e5f0c2ab
aitmil <120422761+aitmil@users.noreply.github.com>
2024-02-25 19:39:00 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const elements = evt.currentTarget.elements;
+  const emailValue = elements.email.value.trim();
+  const passValue = elements.password.value.trim();
+  const info = {
+    email: emailValue,
+    password: passValue,
+  };
+
+  if (emailValue === "" || passValue === "") {
+    alert("All form fields must be filled in");
+  } else {
+    console.log(info);
+  }
+
+  evt.currentTarget.reset();
+}
