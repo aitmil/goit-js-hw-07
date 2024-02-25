3c363acc3dd73bf78c8eb4e022303e992cff9903
aitmil <120422761+aitmil@users.noreply.github.com>
2024-02-25 19:48:45 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", handleClick);
+function handleClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
